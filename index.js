95bcf9289a6fb92e3847d91d3b31fe40243975f8
Cade Rasmussen <caderasmussen@gmail.com>
2021-12-16 15:58:34 -0700

    fixed app.use

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path')
 
 const app = express()
 app.use(express.json())
-app.use('/js'.express.static(path.join(__dirname, "public/main.js")))
+app.use('/js', express.static(path.join(__dirname, "public/main.js")))
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
